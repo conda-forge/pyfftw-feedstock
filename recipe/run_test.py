ab01e9054f9d2198e032e125d5fb2fbb54ff90a6
Gregory Lee <grlee77@gmail.com>
2022-02-19 19:41:01 -0500

    update test script and bump build number

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,11 +1,17 @@
 import numpy as np
 
+import pyfftw
 import pyfftw.builders
 from pyfftw.interfaces.numpy_fft import fftn
-r = np.random.randn(32, 32, 32)
 
 # the following transform will fail if MKL FFT routines are being linked to
 # instead of the FFTW ones.  (MKL doesn't support FFT of only 1
 # dimension of a 3D array).
 # see:  https://github.com/pyFFTW/pyFFTW/issues/40
+r = np.random.randn(32, 32, 32)
 fftn(r, axes=(0, ))
+
+# the following transform will fail if static linking is broken as in
+# https://github.com/pyFFTW/pyFFTW/issues/294
+arr = np.random.randint(1, 500, (128, 128)).astype(np.complex64)
+out = pyfftw.FFTW(arr, np.empty_like(arr), axes=(0, 1))()
